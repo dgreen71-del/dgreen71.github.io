f30d4f3c94c3a2039c01e66cf78766ea3d03313d
dgreen71-del <158539120+dgreen71-del@users.noreply.github.com>
2024-05-05 21:14:57 -0400

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,28 @@
+<indexhtml>
+<head>
+  <title>Introduction of Dayjean Green</title>
+  <meta name="viewport" content="width=device-width,initial-scale=1" >
+  <link href="style.css" rel="stylesheet">
+</head>
+<body>
+  <h1>Hello World!</h1>
+  <p>My name is Dayjean Green and this is my introduction!</p>
+
+  <script src="script.js"></script>
+</body>
+<p>This website is about me and what I do on a daily.
+  </p>
+<h2> About Me</h2>
+<p>My full name is Dayjean Lee Sun Green I was born in 2003, on June 20th. I graduated highschool in 2021 and started my college career studying Automotive. I recently changed what I was studying to a more technological based program since I love Technology. I am now studying Software Developement and have started a few classes for the basics of coding.
+  </p>
+    <h2>My Life Growing Up</h2>
+      <p>My family and I are originally from Flint, Michigan. We moved to Pheonix, Arizona in 2008 and lived there for about half a year. It sucked as a kid since I started school in an Unfamiliar area but life was good. The biggest Event that happened in Pheonix was us visiting the Grand Canyon National park. Shortly after that we ended up moving to Concord, North Carolina. We live in Concord for about 7 years it was hard inetionally moving to a different area and making new freinds but we managed. The crime in the area started to rise rapidaly and eventaully our neighborhood wasnt safe anymore. So we moved to Charlotte North Carolina and have been here ever since.
+        </p>
+<h2>Hobbies
+  </h2>
+    <p> My favorite thing to do in my free time is play Video Games. The main game I play is  <a href= "https://www.bungie.net/7">Destiny</a> I have dedicated a lot of time to this game and developing progress through out missions. If you have never heard of the game Destiny it is an action packed MMO RPG based in space. You the charecter are called gaurdians and you protect the galaxy from evil!
+    </p>
+	<ul>
+		<li><a href="work.css">Work</a></li>
+		<li><a href="family.ss">Family</a></li>
+    </indexhtml>
---
+++ family.js
@@ -0,0 +1,50 @@
+<!DOCTYPE htm>
+<familyhtml>
+  <head>
+  <title> My Family</title>
+</head>
+<body>
+<h1>My Family</h1>
+<nav>
+  <ul>
+    <li><a href="Mom">Mom</a></li>
+    <li><a href="Older Brother">Dayveon</a></li>
+    <li><a href="Younger Brother">Daylani</a></li>
+    <li><a href="Grandmother">Grandma</a></li>
+    <li><a href="Girlfriend">Girlfriend</a></li>
+    <li><a href="Dogs">Dozzer and Doug</a></li>
+    </ul>
+    </nav>
+    <div id="Mom">
+      <h2>About My Mom</h2>
+<p>My Mothers Full name is Dana Overlay she was born in 1979, on July 11th. My Mom is a wonderful person she raised 3 boys with the help of her Mother (My Grandma) at a young age. She graduated college with a Masters in accoutning from the University of Michigan in 2004. 
+  My Mom has worked for the City of Tega Kay for almost 10 years now and still loves her job and the souronding enviroment she works in. 
+  </p>
+ </div>
+ <div id="Older Brother">
+  <h2>About My Older Brother Dayveon</h2>
+  <p>My older brothers full name is Dayveon Green he was born in the year 2000, on September 5th. My older brother has been my biggest role model from the moment I can remeber. He was the first male to graduate highschool in our family within the past 10 years and has a huge passion for the sport of basketball. My brother Dayveon supports my younger brother and I through any hardship we face and I cannot have asked for a better person to be my Older sibling.
+    </p>
+  </div>
+  <div id="Younger Brother">
+    <h2>About My Younger Brother Daylani</h2>
+    <p>My younger brothers full name is Daylani Green he was born in 2005, on November 26th. Daylani is graduating high school this year and I cannot be more proud of the young man he has become. He will be enrolling into UNC with a full ride Jordan Scholarship starting in Fall of 2024. Daylani for sure is the genius of the 3 of us boys graduating with a 4.1 G.P.A and jumping straight into collge out of highschool.
+    </p>
+    </div>
+   <div id="Grandmother">
+  <h2>About My Grandmother</h2>
+  <p>My Grandma's full name is Etta Overlay she was born in 1959, on Febuary 25th. My Grandma helped raise the 3 of us boys along with my mom and I look up to my Grandma as a second mom. My Grandmother Lived a wonderful life by working as a nurse for almost 40 years! Unfortuantly she passed away recently and it is hard to keep moving in life but I know we have her watching over and protecting us.
+  </p>
+   </div>
+   <div id="Girlfriend">
+  <h2> About My Wonderful Girlfreind Lisa</h2>
+  <p>My Girlfreinds Full name is Lisa Kelly she was born in 2003, on July 9th. My girlfriend and I met in 2018 in automotive class in highschool. We later started dating in Novmeber of 2019. The past 4 years I have spent with her has been wonderful. She is a kind and loving person and Absolutly loves the Movie Home Alone. I cannot tell you how many times she has watched the movie just from time we started dating. 
+  </p>
+   </div>
+    <div id="Dogs">
+     <h2>About My Dogs Dozer and Doug</h2>
+     <p>I have 2 dogs Named Dozer and Doug. Dozzer is a full Siberian husky that is about 5 foot standing on his back legs. Dozer is 4  years old and is very big for his breed but is an awesome dog. Doug is a full Golden Doodle he is about 2 years old and is super attention focused. Doug loves attention from anybody and everybody and gets super jealous when Dozer receives attention of him.
+      </p>
+      </div>
+  </body>
+  </familyhtml>
